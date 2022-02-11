abda5dd4757bd1d7a6c4aa0c923de4351b5af50d
netonblpb <skullbog@gmail.com>
2022-02-11 15:08:51 +0300

    Task27 complete

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Определить количество цифр в числе
+
+Console.Clear();
+Console.Write("Ввведите число: ");
+int i = int.Parse(Console.ReadLine());
+int res = i;
+int n = 0;
+while (i > 0)
+{
+      i = i  / 10;
+      n++;
+}
+
+Console.WriteLine($"В числе {res} - {n} цифр.");
